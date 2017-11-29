db747e543e9b5168651dcb86392f556fd3f49c4b
kosugikun <33971692+kosugikun@users.noreply.github.com>
2017-11-29 21:05:00 +0900

    Update promo-strictthemes.php
    支援についての詳しい説明はこちら

--- views/promo-strictthemes.php
+++ views/promo-strictthemes.php
@@ -1,9 +1,9 @@
-<strong><?php _e('プレミアムワードプレスのテーマはWPメンテナンスモードの作者から！', $this->plugin_slug); ?></strong>
+<strong><?php _e('プラグイン開発者に支援をお願いします！', $this->plugin_slug); ?></strong>
 
 <br /><br />
-	<?php _e('WPメンテナンスモードのプラグインが欲しい！これで、ThemeForestでWordPressテーマを直接購入できます。私たちはStrictThemesを再構築し、最高のデザインとコード化されたWordPressテーマを作成します。', $this->plugin_slug); ?>
+	<?php _e('支援はAmazonギフト券でする事が出来ます。詳しくは下のボタンからお願いします。', $this->plugin_slug); ?>
 <br /><br />
 
-<a class="button button-primary" href="https://themeforest.net/user/strictthemes/portfolio?utf8=%E2%9C%93&order_by=sales&ref=StrictThemes" target="_blank">
-	<?php _e('See our WordPress Themes!', $this->plugin_slug); ?>
+<a class="button button-primary" href="https://mcpenano.net/donation/" target="_blank">
+	<?php _e('支援についての詳しい説明はこちら', $this->plugin_slug); ?>
 </a>
