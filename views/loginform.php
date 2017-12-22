c394c11f423394212220f554e92a4f47ef412f73
kosugikun <33971692+kosugikun@users.noreply.github.com>
2017-12-22 17:45:17 +0900

    Update loginform.php

--- views/loginform.php
+++ views/loginform.php
@@ -1,9 +1,9 @@
 <form name="loginform" class="login_form" action="<?php echo esc_url(site_url('wp-login.php', 'login_post')); ?>" method="post">
-    <input type="text" placeholder="username" name="log" />
-    <input type="password" placeholder="password" name="pwd" />
+    <input type="text" placeholder="ユーザー名" name="log" />
+    <input type="password" placeholder="パスワード" name="pwd" />
 
     <?php if (!empty($redirect)) { ?>
         <input type="hidden" name="redirect_to" value="<?php echo $redirect; ?>" />
     <?php } ?>
-    <input type="submit" value="<?php _e('Login', 'wp-maintenance-page'); ?>" />
+    <input type="submit" value="<?php _e('ログイン', 'wp-maintenance-page'); ?>" />
 </form>
