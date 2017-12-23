e2e5a2a84171fb4ac7f2b34cc7de6009c94c6834
kosugikun <33971692+kosugikun@users.noreply.github.com>
2017-12-23 10:16:01 +0900

    Update contact.php

--- views/contact.php
+++ views/contact.php
@@ -23,7 +23,7 @@
                         </tr>
                         <tr>
                             <td style="text-align:center;">
-                                <img src="<?php echo WPMM_IMAGES_URL . 'icon-48.png'; ?>" />
+                                <img src="<?php echo WPMP_IMAGES_URL . 'icon-48.png'; ?>" />
                             </td>
                         </tr>
                         <tr>
@@ -68,7 +68,7 @@
                                             </td>
                                         </tr>
 
-										<?php do_action('wpmm_contact_template_before_message'); ?>
+										<?php do_action('wpmp_contact_template_before_message'); ?>
 
                                         <tr>
                                             <td height="30"></td>
@@ -85,9 +85,9 @@
                                             </td>
                                         </tr> 
 
-										<?php do_action('wpmm_contact_template_after_message'); ?>
+										<?php do_action('wpmp_contact_template_after_message'); ?>
 
-										<?php do_action('wpmm_contact_template_end'); ?>
+										<?php do_action('wpmp_contact_template_end'); ?>
                                     </tbody>
                                 </table>
                             </td>
