ea4510b2af2015ec26d6ee75a1a88e04d9eed7ec
kosugikun <33971692+kosugikun@users.noreply.github.com>
2017-12-23 19:55:53 +0900

    Add files via upload

--- assets/js/scripts-admin-global.js
+++ assets/js/scripts-admin-global.js
@@ -4,11 +4,11 @@ jQuery(function($) {
      * 
      * @since 2.0.4
      */
-    $('.wpmm_notices').on('click', '.notice-dismiss', function() {
+    $('.wpmp_notices').on('click', '.notice-dismiss', function() {
         var notice_key = $(this).parent().data('key');
         
         $.post(ajaxurl, {
-            action: 'wpmm_dismiss_notices',
+            action: 'wpmp_dismiss_notices',
             notice_key: notice_key
         }, function(response) {
             if (!response.success) {
