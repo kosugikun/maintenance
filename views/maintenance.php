023db10db202a4a825e493516675943a8bde867e
kosugikun <33971692+kosugikun@users.noreply.github.com>
2017-12-24 21:29:35 +0900

    Update maintenance.php
    完

--- views/maintenance.php
+++ views/maintenance.php
@@ -85,13 +85,13 @@
 						<form class="contact_form">
 							<?php do_action('wpmp_contact_form_start'); ?>
 
-							<p class="col"><input type="text" placeholder="<?php _e('名前', $this->plugin_slug); ?>" data-rule-required="true" data-msg-required="<?php esc_attr_e('この項目は必須です。', $this->plugin_slug); ?>" name="name" class="name_input" /></p>
-							<p class="col last"><input type="text" placeholder="<?php _e('Eメール', $this->plugin_slug); ?>" data-rule-required="true" data-rule-email="true" data-msg-required="<?php esc_attr_e('この項目は必須です。', $this->plugin_slug); ?>" data-msg-email="<?php esc_attr_e('Please enter a valid email address.', $this->plugin_slug); ?>" name="email" class="email_input" /></p>
+							<p class="col"><input type="text" placeholder="<?php _e('名前', $this->plugin_slug); ?>" data-rule-required="true" data-msg-required="<?php esc_attr_e('この項目は必須です。', 'wp-maintenance-page'); ?>" name="name" class="name_input" /></p>
+							<p class="col last"><input type="text" placeholder="<?php _e('Eメール', $this->plugin_slug); ?>" data-rule-required="true" data-rule-email="true" data-msg-required="<?php esc_attr_e('この項目は必須です。', 'wp-maintenance-page'); ?>" data-msg-email="<?php esc_attr_e('Please enter a valid email address.', $this->plugin_slug); ?>" name="email" class="email_input" /></p>
 							<br clear="all" />
 
 							<?php do_action('wpmp_contact_form_before_message'); ?>
 
-							<p><textarea placeholder="<?php _e('あなたのメッセージ', $this->plugin_slug); ?>" data-rule-required="true" data-msg-required="<?php esc_attr_e('この項目は必須です。', $this->plugin_slug); ?>" name="content" class="content_textarea"></textarea></p>
+							<p><textarea placeholder="<?php _e('あなたのメッセージ', $this->plugin_slug); ?>" data-rule-required="true" data-msg-required="<?php esc_attr_e('この項目は必須です。', 'wp-maintenance-page'); ?>" name="content" class="content_textarea"></textarea></p>
 
 							<?php do_action('wpmp_contact_form_after_message'); ?>
 
@@ -102,12 +102,12 @@
 					</div>
 				</div>                
 
-				<a class="contact_us" href="javascript:void(0);" data-open="<?php echo esc_attr($open); ?>" data-close="<?php echo esc_attr($close); ?>"><?php _e('お問い合わせ', $this->plugin_slug); ?></a>
+				<a class="contact_us" href="javascript:void(0);" data-open="<?php echo esc_attr($open); ?>" data-close="<?php echo esc_attr($close); ?>"><?php _e('お問い合わせ', 'wp-maintenance-page'); ?></a>
 			<?php } ?>
 
 			<?php if (!empty($this->plugin_settings['general']['admin_link']) && $this->plugin_settings['general']['admin_link'] == 1) { ?>
 				<div class="author_link">
-					<a href="<?php echo admin_url(); ?>"><?php _e('ダッシュボード', $this->plugin_slug); ?></a>
+					<a href="<?php echo admin_url(); ?>"><?php _e('ダッシュボード', 'wp-maintenance-page'); ?></a>
 				</div>
 			<?php } ?>    
         </div>
