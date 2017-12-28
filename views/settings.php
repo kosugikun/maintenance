456693cce1c910fa73cfdf7a400df68e47b98805
kosugikun <33971692+kosugikun@users.noreply.github.com>
2017-12-29 08:51:44 +0900

    Update settings.php

--- views/settings.php
+++ views/settings.php
@@ -471,9 +471,31 @@
                             <tbody>
                                 <tr valign="top">
 									<th><?php _e('プラグインバージョン', 'wp-maintenance-page'); ?></th>
-									<td><?php _e('WP Maintenance page', 'wp-maintenance-page'); ?>
+									<td>
+			<?php $plugin_data = wpmm_plugin_info($this->plugin_slug); ?>
+            <ul>
+                <li><?php _e('Name', $this->plugin_slug); ?>: 
+					<?php
+					echo!empty($plugin_data['Name']) ? $plugin_data['Name'] : '';
+					echo!empty($plugin_data['Version']) ? ' v' . $plugin_data['Version'] : '';
+					?>
+                </li>
+                <li><?php _e('Author', $this->plugin_slug); ?>: <?php echo!empty($plugin_data['AuthorName']) ? $plugin_data['AuthorName'] : ''; ?></li>
+                <li><?php _e('Website', $this->plugin_slug); ?>: <?php echo!empty($plugin_data['AuthorURI']) ? '<a href="' . $plugin_data['AuthorURI'] . WPMM_AUTHOR_UTM . '" target="_blank">' . $plugin_data['AuthorName'] . '</a>' : ''; ?></li>
+                <li><?php _e('Twitter', $this->plugin_slug); ?>: <?php echo!empty($plugin_data['Twitter']) ? '<a href="http://twitter.com/' . $plugin_data['Twitter'] . '" target="_blank">@' . $plugin_data['Twitter'] . '</a>' : ''; ?></li>
+                <li><?php _e('GitHub', $this->plugin_slug); ?>: <?php echo!empty($plugin_data['GitHub Plugin URI']) ? '<a href="' . $plugin_data['GitHub Plugin URI'] . '" target="_blank">' . basename($plugin_data['GitHub Plugin URI']) . '</a>' : ''; ?></li>
+										<ul>
+							</td>
+				<td>
+					<?php
+    include("#");        //　HTMLファイル読み込み
+?>
+				</td>
 								</tr>
-						
+							</tbody>
+						</table>
+					</form>
+				</div>
             </div>
         </div>
 
