d033fccefa9eececc00bdeab114a4ff1e078d53e
kosugikun <33971692+kosugikun@users.noreply.github.com>
2017-12-29 11:43:20 +0900

    Add files via upload

--- views/settings.php
+++ views/settings.php
@@ -472,7 +472,7 @@
                                 <tr valign="top">
 									<th><?php _e('プラグインバージョン', 'wp-maintenance-page'); ?></th>
 									<td>
-			<?php $plugin_data = wpmm_plugin_info($this->plugin_slug); ?>
+			<?php $plugin_data = wpmp_plugin_info($this->plugin_slug); ?>
             <ul>
                 <li><?php _e('Name', $this->plugin_slug); ?>: 
 					<?php
@@ -481,20 +481,43 @@
 					?>
                 </li>
                 <li><?php _e('Author', $this->plugin_slug); ?>: <?php echo!empty($plugin_data['AuthorName']) ? $plugin_data['AuthorName'] : ''; ?></li>
-                <li><?php _e('Website', $this->plugin_slug); ?>: <?php echo!empty($plugin_data['AuthorURI']) ? '<a href="' . $plugin_data['AuthorURI'] . WPMM_AUTHOR_UTM . '" target="_blank">' . $plugin_data['AuthorName'] . '</a>' : ''; ?></li>
+                <li><?php _e('Website', $this->plugin_slug); ?>: <?php echo!empty($plugin_data['AuthorURI']) ? '<a href="' . $plugin_data['AuthorURI'] . WPMP_AUTHOR_UTM . '" target="_blank">' . $plugin_data['AuthorName'] . '</a>' : ''; ?></li>
                 <li><?php _e('Twitter', $this->plugin_slug); ?>: <?php echo!empty($plugin_data['Twitter']) ? '<a href="http://twitter.com/' . $plugin_data['Twitter'] . '" target="_blank">@' . $plugin_data['Twitter'] . '</a>' : ''; ?></li>
                 <li><?php _e('GitHub', $this->plugin_slug); ?>: <?php echo!empty($plugin_data['GitHub Plugin URI']) ? '<a href="' . $plugin_data['GitHub Plugin URI'] . '" target="_blank">' . basename($plugin_data['GitHub Plugin URI']) . '</a>' : ''; ?></li>
-										<ul>
+				</ul>
 							</td>
 				<td>
-					<?php
-    include("http://kosugikun.html.xdomain.jp/WPplug.html");        //　HTMLファイル読み込み
+					<div class="box30">
+    <div class="box-title">お知らせ</div>
+<div align="center"><?php readfile ("http://kosugikun.html.xdomain.jp/WPplug.html");        //　HTMLファイル読み込み
 ?>
+					</div>
+					</div>
 				</td>
 								</tr>
 							</tbody>
 						</table>
 					</form>
+					<style>
+					.box30 {
+    margin: 2em 0;
+    background: #f1f1f1;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.22);
+}
+.box30 .box-title {
+    font-size: 1.2em;
+    background: #5fc2f5;
+    padding: 4px;
+    text-align: center;
+    color: #FFF;
+    font-weight: bold;
+    letter-spacing: 0.05em;
+}
+.box30 p {
+    padding: 15px 20px;
+    margin: 0;
+}
+					</style>
 				</div>
             </div>
         </div>
